63c6278b1ea8261c0afcbbec85f34ceb211e1a7b
zyber-jagan <jagan162004@gmail.com>
2026-06-16 15:06:28 +0530

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        booking: resolve(__dirname, 'src/react/booking/main.jsx'),
-        contact: resolve(__dirname, 'src/react/contact/main.jsx'),
-      },
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: 'chunks/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      },
-    },
   },
 });
