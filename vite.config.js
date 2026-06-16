f40b14566dd3669758c9d92481cd84949a44846c
zyber-jagan <jagan162004@gmail.com>
2026-06-16 15:22:17 +0530

    ficx

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        booking: resolve(__dirname, 'booking.html'),
+        gym: resolve(__dirname, 'gym.html'),
+      },
+    },
   },
 });
