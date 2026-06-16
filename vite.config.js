22d6792cb00f66e1af34cdb476baaa8c7025ac79
zyber-jagan <jagan162004@gmail.com>
2026-06-16 14:37:43 +0530

    Update Vite configuration to change output directory from 'assets/react' to 'dist' for build artifacts.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'assets/react',
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
